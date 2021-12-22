e0ce1afc5fa685512dd9adf687e441be57d84a45
GitHub Action <action@github.com>
2021-12-22 12:17:34 +0000

    Template cleanup

--- src/main/kotlin/com/github/lishali12345/intellijplaygroundplugin/MyBundle.kt
+++ src/main/kotlin/com/github/lishali12345/intellijplaygroundplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.lishali12345.intellijplaygroundplugin
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
---
+++ src/main/kotlin/com/github/lishali12345/intellijplaygroundplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.lishali12345.intellijplaygroundplugin.services
+
+import com.github.lishali12345.intellijplaygroundplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
